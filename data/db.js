b1bb3403a2f217ed91491d465dfac50f909073aa
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-10-02 18:26:02 +0200

    boh

--- data/db.js
+++ data/db.js
@@ -3,11 +3,11 @@ const mysql = require("mysql2");
 
 //creo la connessione
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "boolshop_database",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 //stabilisco la connessione al db
