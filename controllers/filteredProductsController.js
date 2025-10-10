7b8002a951566c212dbaf08ec741ed15964bf88a
SergioGBosco <sergiogibosco@gmail.com>
2025-10-10 12:37:48 +0200

    aggiunta rotta per il filtraggio

---
+++ controllers/filteredProductsController.js
@@ -0,0 +1,43 @@
+const connection = require('../data/db');
+
+
+
+// Filtra i prodotti in base a name ,team e size
+
+const indexFilter = (req, res) => {
+
+  const { name, team_name, size } = req.query;
+  let sql = `SELECT p.*, t.team_name, s.size
+FROM products p
+LEFT JOIN teams t ON t.products_id = p.id
+LEFT JOIN sizes s ON s.products_id = p.id
+WHERE 1=1`;
+  const params = [];
+  if (name) {
+    sql += " AND p.name LIKE ?";
+    params.push(`%${name}%`);
+  }
+  if (team_name) {
+    sql += " AND t.team_name = ?";
+    params.push(team_name);
+  }
+  if (size) {
+    sql += " AND s.size = ?";
+    params.push(size);
+  }
+  connection.query(sql, params, (err, results) => {
+    if (err) {
+      return res.status(500).json({ error: `Errore nella query: ${err}` });
+    }
+    res.status(200).json(results);
+  }
+  );
+}
+
+
+
+module.exports = {
+
+  indexFilter
+
+};
